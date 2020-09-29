505eff3e0fea3db9a4fb6d6cce39316c55d2f58b
joao.borges <joao.borges@liera.com.br>
2020-09-28 22:32:30 -0300

    First commit

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suite de teste exmplo com nodeJs", function() {
+
+    it("deve demonstrar o uso do Jasmine com nodeje", function() {
+        expect(true).toEqual(true);
+    });
+
+
+    it('deve demontrar o uso do karma em mode de esculta', function(){
+        expect(1).toEqual(1);
+    });
+})
